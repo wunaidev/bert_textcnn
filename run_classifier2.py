27ff503e10d7b59cd239a4d392185d8c6952a941
wunaidev <wunaidev@foxmail.com>
2020-11-10 13:00:04 +0800

    set seed

--- run_classifier2.py
+++ run_classifier2.py
@@ -607,7 +607,7 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 	with tf.variable_scope("loss"):
 		if is_training:
 			# I.e., 0.1 dropout
-			output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
+			output_layer = tf.nn.dropout(output_layer, keep_prob=0.9, seed=bert_config.random_seed)
 
 		logits = tf.matmul(output_layer, output_weights, transpose_b=True)
 		logits = tf.nn.bias_add(logits, output_bias)
